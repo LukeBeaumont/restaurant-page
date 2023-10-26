b536f27a9adf5acd5c7cc7bcd0f54d609e1c7033
LukeBeaumont <lbeaumont50@msn.com>
2023-10-26 21:16:57 +0100

    Move create tabs functions

--- create-tabs.js
+++ create-tabs.js
@@ -1,6 +1,16 @@
-export default function createTabs() {
+function createTabs() {
   const tab = document.createElement("div");
   tab.classList.add("tabs");
 
   return tab;
+}
+
+export default function appendTabs() {
+  for (let i = 0; i < 3; i++) {
+    container.appendChild(createTabs());
+  }
+  const container = document.createElement("div");
+  container.classList.add("container");
+
+  content.appendChild(container);
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import "./style.css";
 import loadMainPage from "./load-page";
-import createTabs from "../create-tabs";
+import appendTabs from "../create-tabs";
 
 const content = document.getElementById("content");
 
-function appendTabs() {
-  for (let i = 0; i < 3; i++) {
-    container.appendChild(createTabs());
-  }
-}
-const container = document.createElement("div");
-container.classList.add("container");
-
-content.appendChild(container);
 appendTabs();
 content.appendChild(loadMainPage());
