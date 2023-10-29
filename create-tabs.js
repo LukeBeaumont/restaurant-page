26da43e49551f9071a19b45f47e44133d1bbce38
LukeBeaumont <lbeaumont50@msn.com>
2023-10-29 19:56:01 +0000

    Add tab taxt

--- create-tabs.js
+++ create-tabs.js
@@ -1,5 +1,6 @@
-function createTabs() {
+function createTabs(className) {
   const tab = document.createElement("div");
+  tab.classList.add(className);
 
   return tab;
 }
@@ -8,9 +9,14 @@ export default function appendTabs() {
   const content = document.getElementById("content");
   const container = document.createElement("div");
   for (let i = 0; i < 3; i++) {
-    container.appendChild(createTabs());
+    if (i === 0) {
+      container.appendChild(createTabs("home"));
+    } else if (i === 1) {
+      container.appendChild(createTabs("menu"));
+    } else if (i === 2) {
+      container.appendChild(createTabs("contact"));
+    }
   }
-
   container.classList.add("container");
 
   content.appendChild(container);
