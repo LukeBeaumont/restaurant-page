ffb30a0bad9dc494d9f76780815cd2aff58a8cd6
LukeBeaumont <lbeaumont50@msn.com>
2023-11-01 21:27:42 +0000

    Add eventlisteners

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import "./style.css";
 import loadMainPage from "./load-page";
 import appendTabs from "./create-tabs";
+import loadMenu from "./load-menu.js";
 
 appendTabs();
 loadMainPage();
 
 const home = document.querySelector(".home");
-console.log(home);
+const menu = document.querySelector(".menu");
+const contact = document.querySelector(".contact");
 
-home.addEventListener("click", () => {
-  alert("ho");
-});
+function addListeners() {
+  home.addEventListener("click", () => {
+    loadMainPage();
+  });
+  menu.addEventListener("click", () => {
+    loadMenu();
+  });
+}
+
+addListeners();
