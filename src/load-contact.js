e0b5e269a74b5db0ac57ee3035199e2d53a8e4a4
LukeBeaumont <lbeaumont50@msn.com>
2023-11-04 07:37:25 +0000

    Finish contact info

--- src/load-contact.js
+++ src/load-contact.js
@@ -4,7 +4,13 @@ export default function loadContactPage() {
   pageContent.classList.add("page-content");
   content.appendChild(pageContent);
 
+  const address = document.createElement("p");
+  address.innerHTML =
+    "Find us on Melbourne high street at <br> 12 longwalk way,<br>The park,<br>RX3 K9";
+  pageContent.appendChild(address);
+
   const contactInfo = document.createElement("p");
-  contactInfo.textContent = "To book now/enquire please get in touch";
+  contactInfo.innerHTML =
+    "To book now/enquire please get in touch.<br> Call us on 0234 56789 <br> or email: kojiboyresturant@woof.com";
   pageContent.appendChild(contactInfo);
 }
